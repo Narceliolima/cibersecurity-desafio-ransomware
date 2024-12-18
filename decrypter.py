66648847f81da207b266818fca0cb4aafb4ec63a
Narcelio Lima <narcelio.slima@gmail.com>
2024-12-17 22:50:52 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,11 +1,26 @@
 import os
 import pyaes
+import sys
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Verificar se o nome do arquivo foi passado como argumento
+if len(sys.argv) != 2:
+    print("Uso: python decrypter.py <nome_do_arquivo>")
+    sys.exit(1)
+
+# Obter o nome do arquivo do argumento de linha de comando
+file_name = sys.argv[1]
+
+## abrir o arquivo a ser criptografado
+# Abrir, ler e fechar o arquivo
+try:
+    with open(file_name, "rb") as file:
+        file_data = file.read()
+        file.close()
+    print("Arquivo lido com sucesso!")
+except FileNotFoundError:
+    print(f"Erro: O arquivo '{file_name}' não foi encontrado.")
+except Exception as e:
+    print(f"Erro ao ler o arquivo: {e}")
 
 ## chave para descriptografia
 key = b"testeransomwares"
